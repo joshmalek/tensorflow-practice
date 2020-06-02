69adef5bd3ca8da4291fdce382df89e8281f736d
17jmalek98 <17jmalek98@gmail.com>
2020-06-02 15:07:23 -0700

    add neural model

--- sentiment.py
+++ sentiment.py
@@ -1,4 +1,6 @@
 import json
+import numpy as np
+import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -10,6 +12,12 @@ data = [json.loads(line) for line in open("sarcasm_headlines_dataset.json", 'r')
 #    for line in f:
 #        data.append(json.loads(line))
 
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV"
 
 
 sentences = []
@@ -27,7 +35,6 @@ testing_sentences = sentences[training_size:]
 training_labels = labels[0:training_size]
 testing_labels = labels[training_size:]
 
-
 tokenizer = Tokenizer(num_words=vocab_size,oov_token = oov_tok)
 tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
@@ -38,6 +45,24 @@ training_padded = pad_sequences(training_sequences, maxlen=max_length,padding=pa
 testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
 testing_padded = pad_sequences(testing_sequences, maxlen=max_length,padding=padding_type,truncating=trunc_type)
 
-padded = pad_sequences(sequences, padding='post')
-print(padded[0])
-print(padded.shape)
+#apparently need this block to work with TF 2.x
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+#code to create the neural network model, and compile
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+model.summary()
+
+#number of epochs to train the model for (more = better,longer training time)
+#after 30 epochs we have hit 99.23% accuracy
+num_epochs = 30
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
