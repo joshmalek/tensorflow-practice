88f829e862c4495ebd273b95b4570f8e872d9c45
17jmalek98 <17jmalek98@gmail.com>
2020-06-02 14:53:59 -0700

    split into training and testing

--- sentiment.py
+++ sentiment.py
@@ -20,11 +20,24 @@ for item in data:
     labels.append(item['is_sarcastic'])
     urls.append(item['article_link'])
 
-tokenizer = Tokenizer(oov_token = "<OOV>")
-tokenizer.fit_on_texts(sentences)
+# in order to keep our training data seperate from our testing data, we split the data
+training_size = 20000
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+
+tokenizer = Tokenizer(num_words=vocab_size,oov_token = oov_tok)
+tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
 
-sequences = tokenizer.texts_to_sequences(sentences)
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length,padding=padding_type,truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length,padding=padding_type,truncating=trunc_type)
+
 padded = pad_sequences(sequences, padding='post')
 print(padded[0])
 print(padded.shape)
