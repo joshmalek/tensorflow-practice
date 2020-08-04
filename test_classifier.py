1bb4f035a1115a9082974edf4469070f04513a10
Josh Malek <36347275+joshmalek@users.noreply.github.com>
2020-08-04 15:47:37 -0700

    Update test_classifier.py

--- test_classifier.py
+++ test_classifier.py
@@ -1,3 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+
+data = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+
